3671f0b3e199e0966bd05643ac09b122742ee547
hichem <rouishichem@yahoo.fr>
2016-11-28 11:50:55 +0100

    password

---
+++ src/test/java/geco/PasswordGenerationTest.java
@@ -0,0 +1,19 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordGenerationTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRandomPassword() throws Exception {
+
+    }
+}
