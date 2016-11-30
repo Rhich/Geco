2dd022a27189db2c0d7c8de27e352729b11cefb5
hichem <rouishichem@yahoo.fr>
2016-11-30 14:29:28 +0100

    commit Password Generation 2

--- src/main/java/geco/PasswordGeneration.java
+++ src/main/java/geco/PasswordGeneration.java
@@ -1,7 +1,17 @@
 package geco;
 
-/**
- * Created by hichem on 28/11/2016.
- */
+
 public class PasswordGeneration {
+    private StringBuilder sb = new StringBuilder();
+
+    public String getRandomPassword() {
+        int n =8; // how many characters in password
+        String set ="z"; // characters to choose from
+
+        for (int i = 0; i < n; i++) {
+            int k =0;   // random number between 0 and set.length()-1 inklusive
+            sb.append(set.charAt(k));
+        }
+        return sb.toString();
+    }
 }
