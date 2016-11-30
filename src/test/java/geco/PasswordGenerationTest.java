9ba9b537644190a31c8b2fbfbe6c9efa4f339740
hichem <rouishichem@yahoo.fr>
2016-11-30 14:00:24 +0100

    commit Password Generation

--- src/test/java/geco/PasswordGenerationTest.java
+++ src/test/java/geco/PasswordGenerationTest.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PasswordGenerationTest {
+    private PasswordGeneration motDePasse;
 
     @Before
     public void setUp() throws Exception {
+        motDePasse=new PasswordGeneration();
 
     }
 
     @Test
     public void testGetRandomPassword() throws Exception {
+        String res;
+        res=motDePasse.getRandomPassword();
+        assertEquals(8,res.length());
 
     }
 }
